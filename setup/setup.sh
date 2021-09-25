2f86a73772ee149da911b38feaf8dcfc4ecb7a0e
Tyler Hammer <tyler@tylerhammer.com>
2021-09-25 09:14:33 -0700

    Replace download + extract

--- setup/setup.sh
+++ setup/setup.sh
@@ -15,6 +15,34 @@ declare skipQuestions=false
 # | Helper Functions                                                   |
 # ----------------------------------------------------------------------
 
+download() {
+
+    local url="$1"
+    local output="$2"
+
+    if command -v "curl" &> /dev/null; then
+
+        curl -LsSo "$output" "$url" &> /dev/null
+        #     │││└─ write output to file
+        #     ││└─ show error messages
+        #     │└─ don't show the progress meter
+        #     └─ follow redirects
+
+        return $?
+
+    elif command -v "wget" &> /dev/null; then
+
+        wget -qO "$output" "$url" &> /dev/null
+        #     │└─ write output to file
+        #     └─ don't show output
+
+        return $?
+    fi
+
+    return 1
+
+
+
 download_dotfiles() {
 
     local tmpFile=""
@@ -101,6 +129,20 @@ download_utils() {
         && return 0
 
    return 1
+
+}
+
+extract() {
+
+    local archive="$1"
+    local outputDir="$2"
+
+    if command -v "tar" &> /dev/null; then
+        tar -zxf "$archive" --strip-components 1 -C "$outputDir"
+        return $?
+    fi
+
+    return 1
 
 }
 
