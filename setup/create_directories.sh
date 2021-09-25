333724114c8a13285cc69972d032ecce055a1b87
Tyler Hammer <tyler@tylerhammer.com>
2021-09-25 10:22:21 -0700

    Update create_directories.sh

--- setup/create_directories.sh
+++ setup/create_directories.sh
@@ -8,13 +8,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 create_directories() {
 
     declare -a DIRECTORIES=(
-        "$HOME/Documents/development",
-        "$HOME/Documents/development/scripts",
-        "$HOME/Documents/development/sendbird",
-        "$HOME/Documents/development/sendbird/internal",
-        "$HOME/Documents/development/sendbird/samples",
-        "$HOME/Documents/development/sendbird/scripts",
-        "$HOME/Documents/development/sendbird/soleng",
+        "$HOME/Documents/development"
+        "$HOME/Documents/development/scripts"
+        "$HOME/Documents/development/sendbird"
+        "$HOME/Documents/development/sendbird/internal"
+        "$HOME/Documents/development/sendbird/samples"
+        "$HOME/Documents/development/sendbird/scripts"
+        "$HOME/Documents/development/sendbird/soleng"
     )
 
     for i in "${DIRECTORIES[@]}"; do
